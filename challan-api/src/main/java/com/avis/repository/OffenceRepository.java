6abd3fbeab5282075524a0fa5ee512ea0a307f72
Birendra121 <131939251+Birendra121@users.noreply.github.com>
2026-05-21 12:21:02 +0530

    Delete challan-api directory

--- challan-api/src/main/java/com/avis/repository/OffenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.avis.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.avis.util.Offence;
-
-public interface OffenceRepository extends JpaRepository<Offence, Long>{
-
-}
